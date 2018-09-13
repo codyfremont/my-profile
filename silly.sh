5dade508c32cfdba2ad6898aa2415900a8a0caa3
codyfremont <41700866+codyfremont@users.noreply.github.com>
2018-09-13 18:12:53 -0400

    Add files via upload

--- silly.sh
+++ silly.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
 
+sudo yum -y update
+sudo yum -y install httpd mod_ssl
+
+sudo firewall-cmd --permanent --add-port=80/tcp
+sudo firewall-cmd --permanent --add-port=443/tcp
+sudo firewall-cmd --reload
+
+sudo service httpd start
+sudo service httpd status
+
+
 echo "This is a silly script" > /tmp/silly.txt
 
