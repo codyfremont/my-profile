f13472406c7831c761b35eab8260ff9e0dd04147
codyfremont <41700866+codyfremont@users.noreply.github.com>
2018-09-16 16:03:23 -0400

    Update silly.sh
    Git duplicated the file

--- silly.sh
+++ silly.sh
@@ -15,21 +15,3 @@ echo "This is no longer a silly script" > /tmp/silly.txt
 
 #commands found at 
 #https://www.liquidweb.com/kb/how-to-install-apache-on-centos-7/
-=======
-#!/bin/bash
-
-sudo yum -y update
-sudo yum -y install httpd mod_ssl
-
-sudo firewall-cmd --permanent --add-port=80/tcp
-sudo firewall-cmd --permanent --add-port=443/tcp
-sudo firewall-cmd --reload
-
-sudo service httpd start
-sudo service httpd status
-
-
-echo "This is a silly script" > /tmp/silly.txt
-
-#commands found at 
-#https://www.liquidweb.com/kb/how-to-install-apache-on-centos-7/
