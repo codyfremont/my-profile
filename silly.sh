bad059640fd3cefba11410f66e7ba1af2813bfe8
codyfremont <41700866+codyfremont@users.noreply.github.com>
2018-09-16 15:59:49 -0400

    Add files via upload
    Updated Locally

--- silly.sh
+++ silly.sh
@@ -1,3 +1,22 @@
+<<<<<<< HEAD
+#!/bin/bash
+
+sudo yum -y update
+sudo yum -y install httpd mod_ssl
+
+sudo firewall-cmd --permanent --add-port=80/tcp
+sudo firewall-cmd --permanent --add-port=443/tcp
+sudo firewall-cmd --reload
+
+sudo service httpd start
+sudo service httpd status
+
+
+echo "This is no longer a silly script" > /tmp/silly.txt
+
+#commands found at 
+#https://www.liquidweb.com/kb/how-to-install-apache-on-centos-7/
+=======
 #!/bin/bash
 
 sudo yum -y update
@@ -15,3 +34,4 @@ echo "This is a silly script" > /tmp/silly.txt
 
 #commands found at 
 #https://www.liquidweb.com/kb/how-to-install-apache-on-centos-7/
+>>>>>>> refs/remotes/origin/master
